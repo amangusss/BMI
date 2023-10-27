56ba83163269e51aa75e4605cfdfa2f4503deb6f
Nazarkulov Aman <amanaman2004@gmail.com>
2023-10-27 17:27:24 +0600

    Update BMI.java

--- src/BMI.java
+++ src/BMI.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.example.assignment_6;
 
 import javafx.application.Application;
@@ -11,10 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Aman Nazarkulov
- */
 public class BMI extends Application {
 
     @Override
